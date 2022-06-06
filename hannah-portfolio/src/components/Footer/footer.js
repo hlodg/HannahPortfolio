286db7d6cf226f70638db6117c69d3df87befa09
Hannah Lodge <lodg0021@umn.edu>
2022-06-06 08:07:36 -0500

    worked on display properties

--- hannah-portfolio/src/components/Footer/footer.js
+++ hannah-portfolio/src/components/Footer/footer.js
@@ -4,17 +4,15 @@ import './footer.css'
 const Footer = () => {
     return (
         <div>
-            <section >
-                <div class="left-content">
+            <section id="footer">
+                <div class="right-content">
                     <a href="https://github.com/hlodg">
-                    <img src="assets/Github-Mark.png" alt="Github Link Above" />
+                    <img src="assets/Github-Mark.png" alt="Github Link Above" id="gitlink" />
                     </a>
                 </div>
-            </section>
-            <section >
                 <div class="left-content">
                     <a href="https://www.linkedin.com/in/hannah-lodge-79a948a8/">
-                        <img src="assets/Linkedin-logo-png.png" />
+                        <img src="assets/Linkedin-logo-png.png" id="gitlink" />
                     </a>
                 </div>
             </section>
