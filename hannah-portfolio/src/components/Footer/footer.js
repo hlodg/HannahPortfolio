d51c8199ab260531a64589ff826564013cc2e844
Hannah Lodge <lodg0021@umn.edu>
2022-06-05 23:07:55 -0500

    working on linked in

--- hannah-portfolio/src/components/Footer/footer.js
+++ hannah-portfolio/src/components/Footer/footer.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-    </div>
-  )
+    return (
+        <div>
+            <section id="resume">
+                <div class="left-content">
+                    <h2>Github</h2>
+                </div>
+                <div class="right-content">
+                    <div class="content">
+                        <a href="https://github.com/hlodg">GitHub Link</a>
+                    </div>
+                </div>
+            </section>
+            <section id="resume">
+                <div class="left-content">
+                    <h2>LinkedIn</h2>
+                </div>
+                <div class="right-content">
+                    <div class="content">
+                        <a href="https://www.linkedin.com/in/hannah-lodge-79a948a8/">LinkedIn Link</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 
 };
 
