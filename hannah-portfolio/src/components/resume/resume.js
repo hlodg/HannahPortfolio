a62ddbf858f64c4845efd6267c7659e49b9b1021
Hannah Lodge <lodg0021@umn.edu>
2022-06-05 23:25:22 -0500

    working on image tags

--- hannah-portfolio/src/components/resume/resume.js
+++ hannah-portfolio/src/components/resume/resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
     return (
         <div>
             <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://docs.google.com/document/d/1XXB4KOtofsd0luhL30Yff9JhRVmX-pkzTWu3TRoR-mA/edit?usp=sharing" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://docs.google.com/document/d/1XXB4KOtofsd0luhL30Yff9JhRVmX-pkzTWu3TRoR-mA/edit?usp=sharing" allowFullScreen></iframe>
             </div>
         </div>
     )
