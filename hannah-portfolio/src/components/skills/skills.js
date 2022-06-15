916ed45af387e5fff7feb95a49cb1955a980cf09
Hannah Lodge <lodg0021@umn.edu>
2022-06-15 10:11:25 -0500

    added skills

---
+++ hannah-portfolio/src/components/skills/skills.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './skills.css';
+
+const Skills = () => {
+    return (
+        <div>
+            <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
+            <img src="assets/javascript.webp" alt="JavaScript" id="gitlink" />
+            <img src="assets/HTMLCSS.jpg" alt= "CSS" id="gitlink" />
+            <img src="assets/reaction.png" alt= "React" id="gitlink" />
+            <img src="assets/graphql.png" alt= "GraphQL" id="gitlink" />
+            <img src="assets/mongodb-logo.png" alt= "MongoBD" id="gitlink" />
+            <img src="assets/sequelize.png" alt= "Sequelize" id="gitlink" />
+            <img src="assets/sql.png" alt= "SQL" id="gitlink" />
+        </div>
+    )
+
+};
+
+export default Skils;
