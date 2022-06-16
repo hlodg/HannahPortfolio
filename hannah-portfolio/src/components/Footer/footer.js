8732fccd5c98af790b3383452427c893de2a2339
Hannah Lodge <lodg0021@umn.edu>
2022-06-15 20:50:46 -0500

    finished formatting

--- hannah-portfolio/src/components/Footer/footer.js
+++ hannah-portfolio/src/components/Footer/footer.js
@@ -4,12 +4,12 @@ import './footer.css'
 const Footer = () => {
     return (
         <div>
+            <div id= "footer">
+                <h2>Contact Me</h2>
+                <p>hannahlodge00@gmail.com</p>
+                <p>612.242.0166</p>
+            </div>
             <section id="footer">
-                <div>
-                    <h2>Contact Me</h2>
-                    <p>hannahlodge00@gmail.com</p>
-                    <p>612.242.0166</p>
-                </div>
                 <div class="right-content">
                     <a href="https://github.com/hlodg">
                     <img src="assets/mark.png" alt="Github Link Above" id="gitlink" />
--- hannah-portfolio/src/components/skills/skills.js
+++ hannah-portfolio/src/components/skills/skills.js
@@ -5,7 +5,21 @@ const Skills = () => {
     return (
         <div id="skills">
             <h2>Skills:</h2>
-            <div>
+            <ul>
+                <li>JavaScript</li>
+                <li>NodeJS</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>GraphQL</li>
+                <li>MongoDb</li>
+                <li>SQL</li>
+                <li>Sequelize</li>
+                <li>Heroku</li>
+                <li>Rest API</li>
+                <li>Google OAuth</li>
+            </ul>
+            {/* <div>
                 <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
             </div>
             <div>
@@ -28,7 +42,7 @@ const Skills = () => {
             </div>
             <div>
                 <img src="assets/sql.png" alt="SQL" id="gitlink" />
-            </div>
+            </div> */}
         </div>
     )
 
