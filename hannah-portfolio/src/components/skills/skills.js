91c6062c37cfea93d05b3503d09f785d496e0d6c
Hannah Lodge <lodg0021@umn.edu>
2022-06-15 21:17:05 -0500

    fixing format on footer

--- hannah-portfolio/src/components/skills/skills.js
+++ hannah-portfolio/src/components/skills/skills.js
@@ -5,32 +5,32 @@ const Skills = () => {
     return (
         <div id="overallskills">
             <h2>Skills:</h2>
-        <div id="skills">
-            <div>
-                <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
+            <div id="skills">
+                <div>
+                    <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/javascript.webp" alt="JavaScript" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/HTMLCSS.jpg" alt="CSS" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/reacticon.png" alt="React" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/graphql.png" alt="GraphQL" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/mongodb-logo.png" alt="MongoBD" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/sequelize.png" alt="Sequelize" id="gitlink" />
+                </div>
+                <div>
+                    <img src="assets/sql.png" alt="SQL" id="gitlink" />
+                </div>
             </div>
-            <div>
-                <img src="assets/javascript.webp" alt="JavaScript" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/HTMLCSS.jpg" alt="CSS" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/reacticon.png" alt="React" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/graphql.png" alt="GraphQL" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/mongodb-logo.png" alt="MongoBD" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/sequelize.png" alt="Sequelize" id="gitlink" />
-            </div>
-            <div>
-                <img src="assets/sql.png" alt="SQL" id="gitlink" />
-            </div>
-        </div>
         </div>
     )
 
