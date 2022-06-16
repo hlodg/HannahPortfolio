2abdcb93d574fe305f5c1daf4074d0b77a21dc27
Hannah Lodge <lodg0021@umn.edu>
2022-06-15 20:45:51 -0500

    changed format in skills

--- hannah-portfolio/src/components/skills/skills.js
+++ hannah-portfolio/src/components/skills/skills.js
@@ -5,14 +5,30 @@ const Skills = () => {
     return (
         <div id="skills">
             <h2>Skills:</h2>
-            <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
-            <img src="assets/javascript.webp" alt="JavaScript" id="gitlink" />
-            <img src="assets/HTMLCSS.jpg" alt= "CSS" id="gitlink" />
-            <img src="assets/reaction.png" alt= "React" id="gitlink" />
-            <img src="assets/graphql.png" alt= "GraphQL" id="gitlink" />
-            <img src="assets/mongodb-logo.png" alt= "MongoBD" id="gitlink" />
-            <img src="assets/sequelize.png" alt= "Sequelize" id="gitlink" />
-            <img src="assets/sql.png" alt= "SQL" id="gitlink" />
+            <div>
+                <img src="assets/node.jpg" alt="NodeJS" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/javascript.webp" alt="JavaScript" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/HTMLCSS.jpg" alt="CSS" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/reaction.png" alt="React" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/graphql.png" alt="GraphQL" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/mongodb-logo.png" alt="MongoBD" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/sequelize.png" alt="Sequelize" id="gitlink" />
+            </div>
+            <div>
+                <img src="assets/sql.png" alt="SQL" id="gitlink" />
+            </div>
         </div>
     )
 
