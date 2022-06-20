5ff264d0321ef07a5f01430d02ba498004790e63
Hannah Lodge <lodg0021@umn.edu>
2022-06-19 19:26:10 -0500

    added github links

--- hannah-portfolio/src/components/projects/projects.js
+++ hannah-portfolio/src/components/projects/projects.js
@@ -15,6 +15,7 @@ const Projects = () => {
                         <p>This code quiz website enables users to test thier basic knowledge of javascript. By using this information, the user can understand what they need to keep working on.</p>
                         <img src="assets/Code Quiz.gif" alt="https://unsplash.com/s/photos/coming-soon" />
                     </section>
+                    <a href= "https://github.com/hlodg/Code-Quiz">See Code Here</a>
                 </div>
                 <div class="right project">
                     <h3><a href="https://stumartin22.github.io/genre-finder/">Genre Finder</a></h3>
@@ -22,21 +23,24 @@ const Projects = () => {
                         <p>Have you ever been frustrated, not knowing what to listen too? Then the genre finder is for you! The genre finder is an application that enables users to find different albums and artists of a specific genre.</p>
                         <img src="assets/Genre Finder Search Page.gif" alt="https://unsplash.com/s/photos/coming-soon" />
                     </section>
+                    <a href= "https://stumartin22.github.io/genre-finder/">See Code Here</a>
                 </div>
                 <div class="left project">
-                    <h3><a href="https://github.com/RedBread97/golf_tracker">Golf Tracker</a></h3>
+                    <h3><a href="https://morning-bastion-43801.herokuapp.com/">Golf Tracker</a></h3>
                     <section class="project_content">
                         <p>It is important to keep improving your golf game! Therefore, this golf tracker records your scores each game and provides information to help you understand how you are progressing. </p>
                         <img src="assets/golf.jpeg" alt="https://unsplash.com/s/photos/coming-soon" />
                     </section>
+                    <a href="https://github.com/RedBread97/golf_tracker">See Code Here</a>
                 </div>
 
                 <div class="right project">
-                    <h3><a href="https://github.com/fchang1720/Podcast-Website">Podcasts for Kids</a></h3>
+                    <h3><a href="https://radiant-mountain-18459.herokuapp.com/">Podcasts for Kids</a></h3>
                     <section class="project_content">
                         <p>Have your kids ever wanted to create a podcast to share with friends. Well then this is the app for you!</p>
                        <img src="assets/pfk.png" alt="https://unsplash.com/s/photos/coming-soon" />
                     </section>
+                    <a href = "https://github.com/fchang1720/Podcast-Website">See Code Here</a>
                 </div>
 
 
